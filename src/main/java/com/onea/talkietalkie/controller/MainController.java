c23bc10127eb15a14bfdac1546671527417ed5e7
ErickZuniga <ezuniga@1aauto.com>
2019-02-01 14:42:13 -0700

    Changing Indexcontroller name

--- src/main/java/com/onea/talkietalkie/controller/MainController.java
+++ src/main/java/com/onea/talkietalkie/controller/MainController.java
@@ -3,6 +3,7 @@ package com.onea.talkietalkie.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,11 +11,16 @@ import javax.servlet.http.HttpSession;
 import java.security.Principal;
 
 @Controller
-public class IndexController {
-    @RequestMapping("/")
-    String index(Model model, HttpServletRequest request, HttpServletResponse response, Principal principal, HttpSession session) {
-        model.addAttribute("textVariable", "Hello World");
+
+public class MainController {
+    @RequestMapping(value = "/",method = RequestMethod.GET)
+    public String index() {
         return "index";
     }
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String login(){
+        return "login";
+    }
+
 
 }
